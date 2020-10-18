8e06d5e7d263f82868195d26369ea36858c5813c
Kyle Trahan <ktrahan2@gmail.com>
2020-10-18 12:37:15 -0400

    added db info and addscores route

--- main.go
+++ main.go
@@ -8,27 +8,8 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-func returnAllHighScores(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint hit")
-	json.NewEncoder(w).Encode(HighScores)
-}
-
-func handleRequest() {
-	http.HandleFunc("/highscores", returnAllHighScores)
-	http.HandleFunc("/", homePage)
-	username, exists := os.LookupEnv("USERNAME")
-	if exists {
-		log.Println(username)
-	}
-	log.Fatal(http.ListenAndServe(":7000", nil))
-}
 
 func init() {
 	// loads values from .env into the system
@@ -37,20 +18,44 @@ func init() {
 	}
 }
 
-func main() {
-	HighScores = []HighScore{
-		HighScore{ID: 1, Username: "ktrain", Score: 100},
-		HighScore{ID: 2, Username: "nnpalmore", Score: 200},
-	}
-	handleRequest()
-}
-
-//HighScores is an array for all highscores
-var HighScores []HighScore
-
-//HighScore is the structure of the highscore data, primary key gets added differently?
+//HighScore is the structure of the highscore data
 type HighScore struct {
 	ID       int    `json:"ID"`
 	Username string `json:"username"`
 	Score    int    `json:"score"`
+}
+
+func main() {
+	host := os.Getenv("HOST")
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DBNAME")
+	port := os.Getenv("PORT")
+	fmt.Println(host, username, password, dbname, port)
+	handleRequest()
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequest() {
+	http.HandleFunc("/highscores", returnAllHighScores)
+	http.HandleFunc("/addscores", addScores)
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":7000", nil))
+}
+
+func returnAllHighScores(w http.ResponseWriter, r *http.Request) {
+	HighScores := []HighScore{
+		HighScore{ID: 1, Username: "ktrain", Score: 100},
+		HighScore{ID: 2, Username: "nnpalmore", Score: 200},
+	}
+	fmt.Println("Endpoint hit")
+	json.NewEncoder(w).Encode(HighScores)
+}
+
+func addScores(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Add scores hit")
 }
--- models/HighScore.go
+++
@@ -1,8 +0,0 @@
-package models
-
-//HighScore data structure
-type HighScore struct {
-	ID       int    `json:"ID"`
-	Username string `json:"username"`
-	Score    int    `json:"score"`
-}
